b7f929bac00b7ce816ff0c45c210f244452e4b0c
Ricardo <ricardo.gm@live.com>
2023-02-22 19:30:28 -0500

    head script fix

--- src/app/head.js
+++ src/app/head.js
@@ -1,24 +1,10 @@
-import Head from "next/head"
-import Script from "next/script"
-
-export default function CustomHead() {
+export default function Head() {
   return (
     <>
-      <Head>
-        <title>Neutrino AI</title>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta name="neutrio AI" content="generate backend apps with only a text description" />
-        <link rel="icon" href="/neutrino.png" />
-        <Script type="text/javascript">
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "fygheevc1g");
-          `}
-        </Script>
-      </Head>
+      <title>Neutrino AI</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="neutrio AI" content="generate backend apps with only a text description" />
+      <link rel="icon" href="/neutrino.png" />
     </>
   )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './globals.css'
 
 export default function RootLayout({ children }) {
@@ -8,6 +9,15 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
+      <Script id="clarity-analytics" type="text/javascript">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "fygheevc1g");
+          `}
+        </Script>
       <body>{children}</body>
     </html>
   )
